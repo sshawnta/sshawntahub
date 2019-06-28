e93bba757d39b4b296ab683a477089c554862ce0
Shira Shawnta <sshawnta@oa-j5.21-school.ru>
2019-06-28 18:12:14 +0300

    12

--- Push_swap/push_swap.c
+++ Push_swap/push_swap.c
@@ -1,4 +1,3 @@
-#include "ft_printf/ft_printf.h"
 #include "push_swap.h"
 
 int main(int argc, char **argv)
@@ -10,7 +9,7 @@ int main(int argc, char **argv)
 	check_err(&point, argc, argv);
 	create_stacks(&point, argc, argv);
 	sort(&point);
-	disp_moves(&point);
+	show_move(&point);
 	return (0);
 }
 
@@ -26,23 +25,52 @@ void check_err(t_env *point, int argc, char **argv)
 
 	point->arg.name = 0;
 	point->arg.color = 0;
+	point->arg.file = 0;
 
 	while (++j <= 2 && j < argc)
 	{
 		!ft_strcmp(argv[j], "-v") && (point->arg.name = 1) ? i++: 0;
 		!ft_strcmp(argv[j], "-c") && (point->arg.color = 1) ? i++: 0;
+		!ft_strcmp(argv[j], "-f") && (point->arg.file = 1) ? i++: 0;
+
 	}
-	while (++i < argc)
+	if (point->arg.file >0)
+				file_read(point, argc, argv);
+	else
 	{
-		k = -1;
-		while (argv[i][++k])
-			error(!(!ft_isdigit(argv[i][k]) && argv[i][k] != '-'));
-		j = i;
-		val = atof(argv[i]); // заменить на функцию из либы
-		while (++j < argc)
-			error(!(val == ft_atof(argv[j])));
-			error(!(val > MAX_INT || val < MIN_INT));
+		while (++i < argc)
+		{
+			k = -1;
+			while (argv[i][++k])
+				error(!(!ft_isdigit(argv[i][k]) && argv[i][k] != '-'));
+			j = i;
+			val = ft_atof(argv[i]); // заменить на функцию из либы
+			while (++j < argc)
+				error(!(val == ft_atof(argv[j])));
+				error(!(val > MAX_INT || val < MIN_INT));
+		}
 	}
+	
+}
+
+int file_read(t_env *point, int argc, char **argv)
+{
+	//int  i = 0;
+	point->arg.file = 0;
+	int fd;
+	char *line;
+	int res;
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		while ((res = get_next_line(fd, &line)) > -1)
+		{
+			ft_printf("%s", line);
+		}
+	}
+	else
+		error(0);
+	return (0);
 }
 
 void create_stacks(t_env *point, int argc, char **argv)
@@ -73,7 +101,7 @@ void	error(int err)
 {
 	if (err == 0 || err == -1)
 	{
-		ft_putstr("Error\n");
+		ft_printf("%s\n", "Error");
 		exit(0);
 	}
 	return ;
@@ -93,7 +121,7 @@ int swap_a(t_env *point, int mv)
 		tmp = point->stack_a->value;
 		point->stack_a->value = point->stack_a->next->value;
 		point->stack_a->next->value = tmp;
-		mv == 0 ? add_move(point, "sa") : 0; // NEt takoi funkc;
+		mv == 0 ? add_move(point, "sa") : 0;
 		return (1);
 	}
 	return (0);
@@ -108,20 +136,20 @@ int swap_b(t_env *point, int mv)
 		tmp = point->stack_b->value;
 		point->stack_b->value = point->stack_b->next->value;
 		point->stack_b->next->value = tmp;
-		mv == 0 ? add_move(point, "sb") : 0; // NEt takoi funkc;
+		mv == 0 ? add_move(point, "sb") : 0;
 		return (1);
 	}
 	return (0);	
 }
 
-int swap_a_and_b(t_env *point)
+void swap_a_and_b(t_env *point)
 {
 	int opachki;
 
 	opachki = 0;
 	opachki += swap_a(point, 1);
 	opachki += swap_b(point, 1);
-	opachki != 0 add_move(point, "ss") : 0;
+	opachki != 0 ? add_move(point, "ss") : 0;
 }
 
 /*
@@ -169,7 +197,7 @@ void rotate_a_and_b(t_env *point)
 	opachki = 0;
 	opachki += rotate_a(point, 1);
 	opachki += rotate_b(point, 1);
-	opachki != 0 add_move(point, "rr") : 0;
+	opachki != 0 ? add_move(point, "rr") : 0;
 }
 
 /*
@@ -187,10 +215,10 @@ int rev_rotate_a(t_env *point, int mv)
 		while (tmp->next != point->stack_end_a)
 			tmp = tmp->next;
 		point->stack_end_a = tmp;
-		point->stack_end_a->next != NULL;
+		point->stack_end_a->next = NULL;
 		end->next = point->stack_a;
 		point->stack_a = end;
-		mv = 0 ? add_move (point, "rra") : 0;
+		mv == 0 ? add_move (point, "rra") : 0;
 		return (1);
 	}
 	return (0);
@@ -208,10 +236,10 @@ int rev_rotate_b(t_env *point, int mv)
 		while (tmp->next != point->stack_end_b)
 			tmp = tmp->next;
 		point->stack_end_b = tmp;
-		point->stack_end_b->next != NULL;
+		point->stack_end_b->next = NULL;
 		end->next = point->stack_b;
 		point->stack_b = end;
-		mv = 0 ? add_move (point, "rra") : 0;
+		mv == 0 ? add_move (point, "rrb") : 0;
 		return (1);
 	}
 	return (0);
@@ -275,4 +303,221 @@ void add_move(t_env *point, char *mv)
 		point->move_end = point->move;
 		point->count = 1;
 	}
+	else
+	{
+		error((int)(point->move_end->next = (t_move*)malloc(sizeof(t_move))));
+		point->move_end = point->move_end->next;
+		point->count++;
+	}
+	point->move_end->name = mv;
+	point->move_end->next = NULL;
+	if (point->arg.name)
+	{
+		show_stack(point, point->stack_a, 'a');
+		show_stack(point, point->stack_b, 'b');
+		point->arg.color == 0 ? ft_printf("%s%s\n", "Use -> ", mv) : ft_printf("\e[93m%s%s\n","Use - ", mv);
+	}
+}
+
+void show_stack(t_env *point, t_stack *stack, char c)
+{
+	t_stack *st;
+
+	st = stack;
+	c == 'a' && point->arg.color ? ft_putstr(COLOR_GREEN) : 0;
+	c == 'b' && point->arg.color ? ft_putstr(COLOR_BLUE) : 0;
+	ft_printf("%c%s", c, ": ");
+	while (st != NULL)
+	{
+		ft_printf("%d", st->value);
+		st->next != NULL ? ft_printf("%s", ", ") : 0;
+		st = st->next;
+	}
+	ft_printf("\n");
+	ft_printf(COLOR);
+}
+
+void show_move(t_env *point)
+{
+	t_move *st;
+
+	st = point->move;
+	while (st != NULL)
+	{
+		point->arg.color ? color(st->name) : 0;
+		ft_printf("%s", st->name);
+		st->next != NULL ? ft_printf("\n") : 0;
+		st = st->next;
+		ft_printf(COLOR);
+	}
+	ft_printf("\n");
+}
+
+void color(char *name)
+{
+	!ft_strcmp(name, "sa") ? ft_printf(COLOR_RED) : 0;
+	!ft_strcmp(name, "sb") ? ft_printf(COLOR_GREEN) : 0;
+	!ft_strcmp(name, "pa") ? ft_printf(COLOR_YELLOW) : 0;
+	!ft_strcmp(name, "pb") ? ft_printf(COLOR_BLUE) : 0;
+	!ft_strcmp(name, "ra") ? ft_printf(COLOR_MAJ) : 0;
+	!ft_strcmp(name, "rb") ? ft_printf(COLOR_CYAN) : 0;
+	!ft_strcmp(name, "rra") ? ft_printf(COLOR_MAJ) : 0;
+	!ft_strcmp(name, "rrb") ? ft_printf(COLOR_GREY) : 0;
+}
+
+/*
+novyi fail LIBA
+*/
+double	ft_atof(const char *str)
+{
+	int		sign;
+	double	i;
+	double	value;
+
+	i = 1;
+	sign = 0;
+	value = 0;
+	while (ft_isblank(*str))
+		str++;
+	*str == '-' ? sign = 1 : 0;
+	*str == '-' || *str == '+' ? str++ : 0;
+	while ('0' <= *str && *str <= '9')
+		value = value * 10 + (*str++ - '0');
+	if (*str == '.' && (str++))
+		while ('0' <= *str && *str <= '9')
+			value += (*str++ - '0') / (i *= 10);
+	return (sign == 1 && value > 0 ? -value : value);
+}
+
+int	ft_isdigit(int c)
+{
+	return ('0' <= c && c <= '9');
+}
+
+int		ft_isblank(char c)
+{
+	return ((c == ' ' || c == '\n' || c == '\v' ||
+			c == '\t' || c == '\r' || c == '\f'));
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			break ;
+		i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	else
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_putstr(char const *s)
+{
+	write(1, s, ft_strlen(s));
+}
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (s == NULL || start > ft_strlen(s) || \
+			!(str = (char*)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (i < len && s[start] != '\0')
+		str[i++] = s[start++];
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	int		i;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(s1);
+	if (!(str = (char*)malloc(sizeof(*str) * (len + 1))))
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/*NOVIY FAIL ALGOS */
+int minax(t_env *point)
+{
+	int count;
+	int count_min;
+	t_stack *stack;
+
+	point->min = MAX_INT;
+	point->max = MIN_INT;
+	count = 1;
+	count_min = 1;
+	stack = point->stack_a;
+	while (stack != NULL)
+	{
+		point->min > stack->value ? count_min = count : 0;
+		point->min > stack->value ? point->min = stack->value : 0;
+		point->max < stack->value ? point->max = stack->value : 0;
+		stack = stack->next;
+		count++;
+	}
+	point->count_min = count_min;
+	count_min > (count / 2) ? point->count_min = ABS((count - count_min + 1)) : 0;
+	count /= 2;
+	return (count_min > count ? 1 : -1);
+}
+
+int sort(t_env *point)
+{
+	if (point->stack_a->next == NULL)
+		return (0);
+	while (proverka_prav(point->stack_a, 0) != 0 || point->stack_b == NULL)
+	{
+		point->m = minax(point);
+		if (point->min == point->stack_a->next->value)
+			swap_a(point, 0);
+		else
+			while (point->count_min-- - 1 > 0)
+				point->m == -1 ? rotate_a(point, 0) : rev_rotate_a(point, 0);
+		if (proverka_prav(point->stack_a, 0) == 0 && point->stack_b == NULL)
+			break ;
+		if (point->stack_b != NULL &&
+			proverka_prav(point->stack_a, 0) == 0 &&
+			proverka_prav(point->stack_b, 1) == 0)
+			break ;
+		push_b(point);
+	}
+	while (point->stack_b != NULL)
+		push_a(point);
+	return (0);	
+}
+int proverka_prav(t_stack *stack, int md)
+{
+	t_stack *st;
+
+	st = stack;
+	while (st->next != NULL)
+	{
+		if (md == 0 && st->value > st->next->value)
+			return (-1);
+		if (md == 1 && st->value < st->next->value)
+			return (-1);
+		st = st->next;
+	}
+	return (0);
 }
--- Push_swap/push_swap.h
+++ Push_swap/push_swap.h
@@ -3,10 +3,24 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include "ft_printf/ft_printf.h"
 
 # define ABS(x) (x < 0 ? -(x) : x)
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
+# define COLOR_YELLOW "\e[93m"
+# define COLOR_CYAN "\e[36m"
+# define COLOR_GREY "\e[90m"
+# define COLOR_BLUE "\e[34m"
+# define COLOR_MAJ "\e[35m"
+# define COLOR_ORANGE "\e[33m"
+# define COLOR_RED "\e[31m"
+# define COLOR_GREEN "\e[32m"
+# define COLOR "\e[0m"
+# define BUFF_SIZE 32
+# define MAX_FD 1024 + 1
 
 enum { ONE, BOTH };
 enum { INC, DECd };
@@ -28,6 +42,7 @@ typedef struct	s_arg
 {
 	short	name;
 	short	color;
+	short file;
 }				t_arg;
 
 typedef struct	s_env
@@ -45,24 +60,60 @@ typedef struct	s_env
 	int		max;
 	int		m;
 }				t_env;
+
 /*Glavnaya tema*/
 void check_err(t_env *point, int argc, char **argv);
 void create_stacks(t_env *point, int argc, char **argv);
 void	error(int err);
+
+/*GNL*/
+int file_read(t_env *point, int argc, char **argv);
+int	get_next_line(const int fd, char **line);
+int		ft_new_line(char **s, char **line, int fd, int ret);
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
 /*swap*/
 int swap_a(t_env *point, int mv);
 int swap_b(t_env *point, int mv);
-int swap_a_and_b(t_env *point);
+void swap_a_and_b(t_env *point);
+
 /*rotate*/
 int rotate_a(t_env *point, int mv);
 int rotate_b(t_env *point, int mv);
 void rotate_a_and_b(t_env *point);
+
 /*rev rotate*/
 int rev_rotate_a(t_env *point, int mv);
 int rev_rotate_b(t_env *point, int mv);
 void rev_rotate_a_and_b(t_env *point);
+
 /*push*/
 void push_a(t_env *point);
 void push_b(t_env *point);
+
+/*util*/
+void add_move(t_env *point, char *mv);
+void show_stack(t_env *point, t_stack *stack, char c);
+void show_move(t_env *point);
+void color(char *name);
+
+/*Liba*/
+double	ft_atof(const char *str);
+int	ft_isdigit(int c);
+int		ft_isblank(char c);
+int	ft_strcmp(const char *s1, const char *s2);
+void	ft_putstr(char const *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+
+
+/*algos*/
+int sort(t_env *point);
+int minax(t_env *point);
+int proverka_prav(t_stack *stack, int md);
 
 #endif
